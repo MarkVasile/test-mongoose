1f4213eb824ced4b621ab331b69d6c056b094dcf
CodeAwareness <codeawareness@gmail.com>
2023-03-17 19:17:03 -0400

    added schema test

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 ## Testing some weird mongoose issues
 
-### Silently ignoring queries
+`yarn dev` will run the two different tests.
+
+### FIRST TEST: Aliases are only sometimes working
 
 I will look deeper into why this happens, but take a look at the index.ts here, we create a repo, then try to find a repo with a query that should return none. And yet, mongoose happily returns the first record it finds. Looking at the debug message it seems our query was completely ignored, and it now finds any record.
 
 Cause: the use of `alias` in the schema makes the query transformer fail with no errors. Removing the alias and working with actual field names will solve this issue and the try-catch will correctly result in a catch error.
+
+### SECOND TEST: Silently ignoring queries
+
+We're trying to update a record, but we have a field that's not defined in the schema (model).
+Mongo native driver will correctly update ZERO records.
+Mongoose library will update ALL records (look at the query it spits out in the debug message).
+
+This happens with DELETE too, not just UPDATE.
--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,36 @@ mongoose.connect(
 ).then(async () => {
   console.info('Connected to MongoDB')
   /* create a repo */
+  await Repo.deleteMany({})
   await Repo.create({ origin: 'something', auth: [{ u: new mongoose.Types.ObjectId() }] })
   const origin = 'github.com/test-mongoose'
   const uid = '123456'
+  console.log('\nFIRST TEST: Mongoose aliases')
   try {
     // Now try to find a repo that does not exist
     const repo = await Repo.findOne({ origin, 'auth.u': uid })
     console.log('REPO found', repo) // BAD: no errors, and we've found a repo that does not correspond to the query; check mongoose debug msg.
+  } catch (err) {
+    console.error(new Error('could not find repo'))
+    console.log(err)
+  }
+
+  console.log('\nSECOND TEST MongoDB: updating elements using mongoDB library')
+  try {
+    const db = mongoose.connection.db
+    await db.collection('Repos').findOneAndUpdate({ origin, outsideSchema: 1 }, { $set: { o: 'BOOM !!' } })
+    const repo = await Repo.findOne({ origin })
+    console.log('REPO after update', repo) // BAD: no errors, and we've found a repo that does not correspond to the query; check mongoose debug msg.
+  } catch (err) {
+    console.error(new Error('could not find repo'))
+    console.log(err)
+  }
+
+  console.log('\nSECOND TEST Mongoose: updating elements')
+  try {
+    await Repo.updateOne({ origin, outsideSchema: 1 }, { $set: { o: 'BOOM !!' } })
+    const repo = await Repo.findOne({ origin })
+    console.log('REPO after update', repo) // BAD: no errors, and we've found a repo that does not correspond to the query; check mongoose debug msg.
   } catch (err) {
     console.error(new Error('could not find repo'))
     console.log(err)
